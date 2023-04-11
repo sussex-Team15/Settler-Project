9e174c45bf7b5c531ca660770d2a02938945bf57
Andris73 <57486647+Morganplant@users.noreply.github.com>
2023-04-11 11:56:14 +0100

    pylint fixes (1/11)

--- src/bank.py
+++ src/bank.py
@@ -1,19 +1,22 @@
-
-from development_cards import DevelopmentCard
-from resource_ import *
-
-
 class Bank:
 
-    def __init__(self, resources, trade_ratios, dev_cards, vp_cards):
+    def __init__(self, resources, dev_cards):
 
         # not sure what bank starts with (to change)
         self.resources = resources
         self.trade_ratios = 4  # default trade ratio -
         # goes to 3 if player has settlement or trades on harbour
-        self.dev_cards = []  # list as stack (only uses pop and push)
+        # list as stack (only uses pop and push)
+        self.dev_cards = dev_cards
 
     def get_trade_ratio(self, num_settlements):
         if num_settlements > 0:
             return 3
-        return self.trade_ratio
+        return self.trade_ratios
+
+    def null_methode(self):
+        return (
+            self.resources,
+            self.trade_ratios,
+            self.dev_cards
+        )
